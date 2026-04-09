56e871de62cdcaaa64048332664003d541815d5e
huanxiao <490327763@qq.com>
2026-04-09 15:57:08 +0800

    3.5

---
+++ 3.5/dash_shell.c
@@ -0,0 +1,16 @@
+// dash_shell.c
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    char *argv[2];
+    argv[0] = "/bin/sh";
+    argv[1] = NULL;
+    
+    // setuid(0);  // Line 11 - COMMENTED OUT initially
+    execve("/bin/sh", argv, NULL);
+    
+    return 0;
+}
